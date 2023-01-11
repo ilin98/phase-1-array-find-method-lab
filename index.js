26ea66677a9928d6fd6519f13dc80b40fd50b7f3
Isaac <isaac.lin.pnc@gmail.com>
2023-01-11 12:06:42 -0500

    finished lab.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    { year: "2012", result: "N/A"},
+  ]
+
+function superbowlWin(array){
+    for(let item of array){
+        if(item.result === "W"){
+            return item.year;
+        }
+    }
+}
+
+console.log(superbowlWin(record));
